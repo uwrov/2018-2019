a596ba707cea5e9db96e2a256c8bc7d6697d9a8c
Peyton L <gabybiscuits@gmail.com>
2019-10-16 19:02:15 -0700

    Video Testing
    Got code for shape detection via video feed working correctly. Needs some fine-tuning in terms of shape recognition.

--- imagerecognition/test_video.py
+++ imagerecognition/test_video.py
@@ -1,17 +1,53 @@
 import numpy as np 
 import cv2
+import imutils
+from shapedetector import ShapeDetector
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-while(True):
+while (True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, image = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #image = cv2.imread(args["image"])
+    image = imutils.resize(image, width = 800)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5,), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    cv2.bitwise_not ( thresh, thresh );
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    sd = ShapeDetector()
+
+    # loop over the contours
+    for c in cnts:
+        # compute the center of the contour, then detect the name of the
+        # shape using only the contour
+
+
+        M = cv2.moments(c)
+        if M["m00"] == 0:
+            continue
+
+        cX = int((M["m10"] / M["m00"]))
+        cY = int((M["m01"] / M["m00"]))
+        shape = sd.detect(c)
+
+        # multiply the contour (x, y)-coordinates by the resize ratio,
+        # then draw the contours and the name of the shape on the image
+        c = c.astype("float")
+        c = c.astype("int")
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+            0.5, (255, 255, 255), 2)
+
+    # show the output image
+    cv2.imshow("Image", image)
+    #cv2.waitKey(0)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
