9ad689bb50b41c928dc6798b1aa9db865d137f75
ajang304 <ajang304@uw.edu>
2019-10-23 20:31:32 -0700

    Added some functionality to vase and vase button
    - Added vaseOnClick();
    - Added spawnVase();

--- Graphics Team/Vase Breaker/main.js
+++ Graphics Team/Vase Breaker/main.js
@@ -2,52 +2,70 @@
 *	This is the main javascript file for the vase breaker project.
 */
 
-let numVases = 0; //increases by one everytime a vase is clicked, and will accumulate
-let time = 0; // increases by one every second until gameState is false
-let gameState = true; // will become false when manager is present, and a vase is clicked
-let managerPresence = false; // appears(and become true) every five seconds, and remains for two seconds
-let vaseSpeed = 5; // takes five seconds to cross the screen
-let lightsFlicker = false; //will be true a second before the managerPresence is true.
-function vaseButton() {
-	//when clicked, broken vase image(Google it)
-	//it will not disappear. it will only stay on the conveyer
-	// every time tshis function is triggered numVase++
-	//hitBox = rectangle
-
-}
-function vaseMovement() {
-	//position time  -> a displacement funtion with respect to time?
-
-}
-function scoreBoard() {
-	//timePassed
-	//score
-}
-function manager() {
-  //managerPresence
-	//lights flicker
-}
 
 
 
-(function() {
+
+//(function() {
 	"use strict";
 
-	//
-	//	Init will run when the web page loads.
-	//
 
-	window.addEventListener("load", init);
+		let numVases = 0; //increases by one everytime a vase is clicked, and will accumulate
+		let time = 0; // increases by one every second until gameState is false
+		let gameState = true; // will become false when manager is present, and a vase is clicked
+		let managerPresence = false; // appears(and become true) every five seconds, and remains for two seconds
+		let vaseSpeed = 5; // takes five seconds to cross the screen
+		let lightsFlicker = false; //will be true a second before the managerPresence is true.
 
-	/**
-    *	Is to be called when the page loads. initiates all the event listener in the starting
-    * 	screen
-    */
+		//
+		//	Init will run when the web page loads.
+		//
+
+		window.addEventListener("load", init);
+
+		/**
+	    *	Is to be called when the page loads. initiates all the event listener in the starting
+	    * 	screen
+	    */
+  		function init() {
+
+  		}
 
 
-  	function init() {
+		function vaseOnClick() {
+			//when clicked, broken vase image(Google it)
+			//it will not disappear. it will only stay on the conveyer
+			// every time tshis function is triggered numVase++
+			//hitBox = rectangle
+			this.parentElement.removeChild(this);
+		}
 
-  	}
+		/**
+		*	spawn random new vase on to the conveyer belt
+		*/
+		function spawnVase() {
+			let vase = document.createElement("button");
+			vase.classList.add("vase");
+			vase.addEventListener("click", vaseOnClick);
+			document.getElementById("vases").appendChild(vase);
+
+		}
 
 
-})();
+		function vaseMovement() {
+			 //position time  -> a displacement funtion with respect to time?
+
+		}
+
+		function scoreBoard() {
+			 //timePassed
+			 //score
+		}
+
+		function manager() {
+		   //managerPresence
+			 //lights flicker
+		}
+
+
+//s})();
