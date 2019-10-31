18c037607685971e8121a1e4136ca11aebdb7ae0
chrisy1225 <chrisy5@uw.edu>
2019-10-30 20:04:39 -0700

    FlipCoin
    We made a flipcoin game on the playground

--- Graphics Team/Html PlayGround/chris.js
+++ Graphics Team/Html PlayGround/chris.js
@@ -5,6 +5,10 @@
 (function() {
 	"use strict";
 
+
+	let balance = 100;
+
+
 	window.addEventListener("load", init);
 
 	/**
@@ -14,6 +18,8 @@
   	function init() {
 		document.getElementById("add").addEventListener("click", add);
 		document.getElementById("subtract").addEventListener("click", subtract);
+		document.getElementById("heads").addEventListener("click", coinFlip);
+		document.getElementById("tails").addEventListener("click", coinFlip);
   	}
 
 	function add() {
@@ -21,6 +27,7 @@
 		let b = document.getElementById("num_b").value;
 		a = parseFloat(a);
 		b = parseFloat(b);
+
 		document.getElementById("ans").innerText = a + b;
 
 	}
@@ -32,4 +39,49 @@
 		b = parseFloat(b);
 		document.getElementById("ans").innerText = a - b;
 	}
+
+	function getBet() {
+		return document.getElementById("bet-input").value;
+	}
+
+	function coinFlip() {
+		console.log("Click!");
+		let bet = getBet();
+		if(bet == null || bet > balance) {
+			cantBet();
+		} else {
+			let guess = this.value;
+			let num = Math.floor(Math.random() * 2);
+			//heads = 1
+			//tails = 0
+			let result;
+			if(num == 0) {
+				result = "tails";
+			} else if(num == 1) {
+				result = "heads";
+			}
+
+			let win = result == guess;
+			if(win) {
+				balance += parseInt(bet);
+				document.getElementById("coin-results").innerText = "You win!";
+			} else {
+				balance -= parseInt(bet);
+				document.getElementById("coin-results").innerText = "You lose!";
+			}
+			updateBalance();
+			document.getElementById("coin-results").innerText += " Flip was " + result +
+												", Your guess was " + guess + "!";
+
+		}
+	}
+
+	function cantBet() {
+		document.getElementById("coin-results").innerText = "You don't have enough money!";
+	}
+
+	function updateBalance() {
+		document.getElementById("balance").innerText = balance;
+	}
+
 })();
