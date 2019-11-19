577924bee460140b98646c329c743048b25a182f
Peter Gunarso <peter@gunar.so>
2019-11-18 18:42:08 -0800

    reorganize readbytes.py

--- sensor/readbytes.py
+++ sensor/readbytes.py
@@ -3,26 +3,23 @@ import time
 import struct
 # Read acceleromoter output from arduino, 
 # expects output to be in space separated string.
-def read_string():
-    ser = serial.Serial('COM3', 115200, timeout=6)
-    while True:
-        time.sleep(.25)
-        ser.flushInput()
-        reading = ser.readline().split()
-        if len(reading) == 3:
-            print("x: " + reading[0].decode('ascii') + 
-                  ", y: " + reading[1].decode('ascii') +
-                  ", z: " + reading[2].decode('ascii'))
-        else:
-            print("poop data")
+def read_string(ser):
+    ser.flushInput()
+    # Wait a little bit for data to be written to serial
+    time.sleep(.25)
+    reading = ser.readline().split()
+    result = None
+    
+    if len(reading) == 3:
+        result = "x: " + reading[0].decode('ascii') + \
+                 ", y: " + reading[1].decode('ascii') + \
+                 ", z: " + reading[2].decode('ascii')
+    else:
+        result = "poopoo"
+    
+    return result
 
 if __name__ == '__main__':
-    # ser = serial.Serial('COM3', 115200, timeout=6)
-    # while True:
-    #     time.sleep(0.25)
-    #     ser.flushInput()
-    #     out = ser.readline()
-    #     if len(out) > 0:
-    #         # print(out[0:-1])
-    #         print(struct.unpack('f', out[0:-1]))
-    read_string()
+    ser = serial.Serial('COM3', 115200, timeout=6)
+    while True:
+        print(read_string(ser))
