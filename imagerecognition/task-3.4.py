4f4882bd035da8feb5aa336ce629e8f50cf7f9f2
margot.adam <margot.adam@gmail.com>
2020-01-06 17:55:52 -0800

    tests to align images

--- imagerecognition/task-3.4.py
+++ imagerecognition/task-3.4.py
@@ -3,10 +3,37 @@ import cv2
 def __main__:
     return
 
-def matrixDifference(img1, img2)
-    
+# align the before and after picture to make sure
+# they are in the same orientation when comparing
+# color
+def alignImages(img1, img2):
+    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY) # REFERENCE IMAGE
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) # IMAGE TO ALIGN WITH REFERENCE
+    height, width = img2.shape
 
-    return
+    # create ORB detecor (to find keypoints later)
+    orb_detector = cv2.ORB_create(5000)
 
-def alignImages():
-    return
+    kp1, d1 = orb_detector.detectAndCompute(img1_gray, None)
+    kp2, d2 = orb_detector.detectAndCompute(img2_gray, None)
+
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+
+    matches = matcher.match(d1, d2)
+
+    matches.sort(key = lambda x: x.distance)
+
+    matches = matches[:int(len(matches)*90)]
+    no_of_matches = len(matches)
+
+    p1 = np.zeros((no_of_matches, 2))
+    p2 = np.zeros((no_of_matches, 2))
+
+    for i in range(len(matches)):
+      p1[i, :] = kp1[matches[i].queryIdx].pt
+      p2[i, :] = kp2[matches[i].trainIdx].pt
+
+    homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
+
+    transformed_img = cv2.warpPerspective(img1_gray, homography, (width, height))
+    return transformed_img
