ce85e1616ad2cc5c96e77ab422f6fc5b89b44ac0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-21 23:02:24 -0800

    add support for network camera

--- imagerecognition/testing.py
+++ imagerecognition/testing.py
@@ -1,3 +1,17 @@
 import cv2
 
-print cv2.__version__
+def main():
+    cap = cv2.VideoCapture('http://10.0.0.27:8080/video/mjpeg')
+
+    while True:
+        _, frame = cap.read()
+        cv2.imshow('feed', frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+    cap.release()
+
+if __name__ == '__main__':
+    main()
