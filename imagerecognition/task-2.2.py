fdc4cfc07d4647e485167f942569054056f4766b
laurenk104 <lkrieger104@gmail.com>
2020-01-22 17:51:51 -0800

    add warp stuff

--- imagerecognition/task-2.2.py
+++ imagerecognition/task-2.2.py
@@ -2,9 +2,13 @@ import cv2
 import numpy as np
 import imutils
 
-RECTANGLE_THRESHOLD = 200
+RECTANGLE_THRESHOLD = 150
+PIXELS_PER_CM = 2
+LENGTH = 110
+HEIGHT = 50
+WIDTH = 50
 
-def getRectangleImage(img):
+def getRectangleImage(img, size):
     """ Converts an image to a cropped, translated image of a rectangle
     :param img: The original image (in color)
     :return: A transformed image cropped to the rectangle (or None if none found)
@@ -15,7 +19,7 @@ def getRectangleImage(img):
     if points is None:
         print("getRectangleImage(): No Rectangle Found.")
         return None
-    processed_img = transformImage(img.copy(), points)
+    processed_img = transformImage(img.copy(), (400, 600), points)
 
 def getRectangle(binary_img):
     """ Finds a rectangle in a binary image and returns its corners (points)
@@ -25,32 +29,41 @@ def getRectangle(binary_img):
     """
 
     #Using cv2.RETR_LIST to prevent nesting contours
-    cnts = cv2.findContours(binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(binary_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
     cnts = imutils.grab_contours(cnts)
     # Sorts contours by size and gets largest contour
-    largest_contour = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+
+    debugImg = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(debugImg, cnts, -1, (255, 0, 255), 3)
+
+    largest_contour = cnts[0]
+
+    epsilon = 0.1*cv2.arcLength(largest_contour,True)
+    approx = cv2.approxPolyDP(largest_contour,epsilon,True)
+
+    cv2.drawContours(debugImg, cnts, -1, (0, 255, 255), 8)
+    cv2.drawContours(debugImg, [approx], -1, (0, 0, 255), 10)
+    cv2.imshow("debug contours", cv2.resize(debugImg, (400, 600)))
 
     # Extra code to visualize the image
     # test_img = cv2.drawContours(cv2.cvtColor(binary_img.copy(), cv2.COLOR_GRAY2BGR), largest_contour, -1, (255, 0, 255), 8)
     # cv2.imshow("getRectangle Contour Map", test_img)
 
-    if len(largest_contour) == 4:
-        return largest_contour
+    if len(approx) == 4:
+        return approx
     return None
 
-def transformImage(image, points):
+def transformImage(image, size, points):
     """ Takes in the points of a rectangle and transforms and crops the image to it.
     :param image: The colored, original image.
+    :param size: The size of the output image as an array [width, height]
     :param points: The array of points representing the corners of the rectangle.
-    :return: Transformed and cropped image.
+    :return: A new image
     """
 
-    """[[[279 274]]
-        [[279 427]]
-        [[440 427]]
-        [[440 274]]]
-
-        How to get points:
+    """ How to get points:
         points[0-3][0][0-1]
                ^ four different points
                        ^ 0 is x and 1 is y
@@ -78,6 +91,40 @@ def transformImage(image, points):
         bottomright = points[2][0]
     print(points)
 
-image = cv2.imread("images/identifytest_new_pink.PNG")
-getRectangleImage(image)
+    quadPoints = np.array([topleft, topright, bottomleft, bottomright], dtype = "float32")
+    width = size[0]
+    height = size[1]
+    finalPoints = np.array([
+        [0, 0],
+        [width, 0],
+        [0, height],
+        [width, height]],
+        dtype = "float32")
+
+    transform = cv2.getPerspectiveTransform(quadPoints, finalPoints) #Get matrix
+    output = cv2.warpPerspective(image, transform, size) #Transform image
+    cv2.imshow("output", output)
+
+
+
+
+#image = cv2.imread("images/test3.JPG")
+end = (WIDTH * PIXELS_PER_CM, HEIGHT * PIXELS_PER_CM)
+side = (LENGTH * PIXELS_PER_CM, HEIGHT * PIXELS_PER_CM)
+top = (LENGTH * PIXELS_PER_CM, WIDTH * PIXELS_PER_CM)
+sizes = [end, side, end, side, top]
+sizesIndex = 0
+outputImages = []
+
+while(sizesIndex < len(sizes)) {
+    #process and show image
+    #Get camera feed => pass to getRectangleImage(feed, sizes[sizesIndex])
+    #save image when clicked
+    if(False): #clicked:
+        #outputImages = image
+        #sizesIndex++
+
+#show images correctly
+
+#getRectangleImage(image)
 cv2.waitKey(0)
