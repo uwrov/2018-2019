78791400729e7c2ce510b213d83946a0bafac5f3
Peyton L <gabybiscuits@gmail.com>
2020-01-22 18:53:29 -0800

    Task-2.2 Prototype Working!

--- imagerecognition/task-2.2.py
+++ imagerecognition/task-2.2.py
@@ -8,18 +8,41 @@ LENGTH = 110
 HEIGHT = 50
 WIDTH = 50
 
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    """ Resizes an image while maintaining aspect ratio.
+
+    """
+
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
 def getRectangleImage(img, size):
     """ Converts an image to a cropped, translated image of a rectangle
     :param img: The original image (in color)
     :return: A transformed image cropped to the rectangle (or None if none found)
+    :return: Points defining the rectangle found
     """
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, binary_img = cv2.threshold(gray_img, RECTANGLE_THRESHOLD, 255, cv2.THRESH_BINARY)
     points = getRectangle(binary_img)
+
     if points is None:
-        print("getRectangleImage(): No Rectangle Found.")
-        return None
-    processed_img = transformImage(img.copy(), (400, 600), points)
+        #print("getRectangleImage(): No Rectangle Found."
+        return None, None
+
+    transformed_img = transformImage(img.copy(), size, points)
+    return transformed_img, points
 
 def getRectangle(binary_img):
     """ Finds a rectangle in a binary image and returns its corners (points)
@@ -30,22 +53,17 @@ def getRectangle(binary_img):
 
     #Using cv2.RETR_LIST to prevent nesting contours
     cnts = cv2.findContours(binary_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
     cnts = imutils.grab_contours(cnts)
+
     # Sorts contours by size and gets largest contour
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
 
-    debugImg = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
-    cv2.drawContours(debugImg, cnts, -1, (255, 0, 255), 3)
+    if len(cnts) == 0:
+        return None
 
     largest_contour = cnts[0]
-
     epsilon = 0.1*cv2.arcLength(largest_contour,True)
     approx = cv2.approxPolyDP(largest_contour,epsilon,True)
-
-    cv2.drawContours(debugImg, cnts, -1, (0, 255, 255), 8)
-    cv2.drawContours(debugImg, [approx], -1, (0, 0, 255), 10)
-    cv2.imshow("debug contours", cv2.resize(debugImg, (400, 600)))
 
     # Extra code to visualize the image
     # test_img = cv2.drawContours(cv2.cvtColor(binary_img.copy(), cv2.COLOR_GRAY2BGR), largest_contour, -1, (255, 0, 255), 8)
@@ -72,7 +90,7 @@ def transformImage(image, size, points):
         point[a][0][b] = [[279, 274], [279, 427], [440, 427], [440, 274]]
                      a =   0           1           2           3
     """
-    sorted(points, key=lambda point: point[0][0])
+    sorted(points, key=lambda point: point[0][0], reverse = True)
 
     # get left side points
     if(points[0][0][1] < points[1][0][1]):
@@ -89,7 +107,7 @@ def transformImage(image, size, points):
     else:
         topright = points[3][0]
         bottomright = points[2][0]
-    print(points)
+    #print(points)
 
     quadPoints = np.array([topleft, topright, bottomleft, bottomright], dtype = "float32")
     width = size[0]
@@ -103,10 +121,10 @@ def transformImage(image, size, points):
 
     transform = cv2.getPerspectiveTransform(quadPoints, finalPoints) #Get matrix
     output = cv2.warpPerspective(image, transform, size) #Transform image
-    cv2.imshow("output", output)
+    return output
 
-
-
+""" Main
+"""
 
 #image = cv2.imread("images/test3.JPG")
 end = (WIDTH * PIXELS_PER_CM, HEIGHT * PIXELS_PER_CM)
@@ -116,15 +134,27 @@ sizes = [end, side, end, side, top]
 sizesIndex = 0
 outputImages = []
 
-while(sizesIndex < len(sizes)) {
-    #process and show image
-    #Get camera feed => pass to getRectangleImage(feed, sizes[sizesIndex])
-    #save image when clicked
-    if(False): #clicked:
-        #outputImages = image
-        #sizesIndex++
+cap = cv2.VideoCapture(1)
 
-#show images correctly
+if(cap.isOpened()):
+    print("Opened camera.")
+    while(sizesIndex < len(sizes)):
+        # Capture frame-by-frame
+        ret, image = cap.read()
+        image = resize(image, width = 800)
+        warpedImage, points = getRectangleImage(image.copy(), sizes[sizesIndex])
 
+        if points is not None and warpedImage is not None:
+            cv2.drawContours(image, [points], -1, (0, 0, 255), 5)
+            shrink_img = resize(warpedImage.copy(), width = 50)
+            rows, columns, channels = shrink_img.shape
+            image[0:rows, 0:columns] = shrink_img
+        cv2.imshow("Camera View", image)
+            #outputImages = image
+            #sizesIndex++
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+else:
+    print("Failed to get camera.")
 #getRectangleImage(image)
-cv2.waitKey(0)
