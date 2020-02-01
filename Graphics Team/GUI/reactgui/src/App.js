d0881f8dbc76419098410c2ec7e3654d873108e1
gboksm11 <56568900+gboksm11@users.noreply.github.com>
2020-02-01 15:35:16 -0800

    fixReact

--- Graphics Team/GUI/reactgui/src/App.js
+++ Graphics Team/GUI/reactgui/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the home page
         </p>
+        <button> Settings </button>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ Graphics Team/GUI/reactgui/src/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class Button extends React.Component {
+
+  render() {
+    return(
+      <div>
+      <img src="" alt="THIS IS TOTALLY AN IMAGE" />
+      <Button variant="primary"> Primary </Button>
+      </div>
+    );
+
+  }
+
+}
+
+export default Button;
