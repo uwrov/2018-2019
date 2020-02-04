157fbd3c2f8469101b392236279976c859685918
margotTheMarvelous <margot.adam@gmail.com>
2020-02-03 17:18:27 -0800

    updated testing for task3.4

--- imagerecognition/task-3.4.py
+++ imagerecognition/task-3.4.py
@@ -82,7 +82,9 @@ def matrix_difference(img1, img2):
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:4]
     for c in cnts:
-        cv2.drawContours(img2, [c], -1, (255, 0, 255), 3)
+        x,y,w,h = cv2.boundingRect(c)
+        if(h/w > 0.3 and h/w < 2.5):
+            cv2.drawContours(img2, [c], -1, (255, 0, 255), 3)
     return img2
 
 def from_binary_color(input):
@@ -101,9 +103,10 @@ def binarize_color(img, boundaries):
 
 
 def binarization(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #what are the lower and upper of HSV?
-    boundariesWhite = [[150, 150, 150], [255, 255, 255]] #change values later!!!
-    boundariesPink = [[140, 50, 140], [255, 125, 255]]
+    boundariesWhite = [[180, 180, 180], [255, 255, 255]] #change values later!!!
+    boundariesPink = [[140, 50, 140], [255, 135, 255]]
     return binarize_color(img, boundariesWhite), binarize_color(img, boundariesPink)
 
 #
@@ -122,13 +125,22 @@ def classify_change_types(ref_w, ref_p, new_w, new_p):
     ref_wp = cv2.bitwise_or(ref_w, ref_p)
     new_wp = cv2.bitwise_or(new_w, new_p)
 
+    #cv2.imshow("ref_wp", ref_wp)
+    cv2.imshow("new_wp", new_wp)
+    
     # Get masks of background color (areas where there is no pink/white)
     ref_b = cv2.bitwise_not(ref_wp) # white = background color
     new_b = cv2.bitwise_not(new_wp)
 
+    cv2.imshow("ref_b", ref_b)
+    
     # Growth: New pink/white in ref background areas
-    growth = cv2.bitwise_and(new_wp, ref_b)
+    #growth = cv2.bitwise_and(new_p, ref_p) one of these was wp
+    #gt = cv2.bitwise_or(new_p, ref_p)
+    gt = new_p - ref_p
 
+    finalGrowth = cv2.bitwise_or(gt, ref_b)
+    cv2.imshow("finalGrowth", finalGrowth)
     # Damage: New background in ref pink/white
     damage = cv2.bitwise_and(new_b, ref_wp)
 
@@ -137,53 +149,83 @@ def classify_change_types(ref_w, ref_p, new_w, new_p):
 
     # Recovery: New pink in ref white
     recovery = cv2.bitwise_and(new_p, ref_w)
-
+    '''
     ret, thresh = cv2.threshold(growth, 127, 255, 0)
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     growth = cv2.cvtColor(growth, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(growth, cnts, -1, (0, 255, 0), 2)
     cv2.imshow("growth", growth)
-
+    '''
+    ret, thresh = cv2.threshold(gt, 127, 255, 0)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    gt = cv2.cvtColor(gt, cv2.COLOR_GRAY2BGR)
+    '''for c in cnts:
+        xP,yP,wP,hP = cv2.boundingRect(c)
+        if(wP * hP >= 1 and wP*hP <= 3):
+            print(wP*hP)
+            cv2.drawContours(gt, cnts, -1, (0, 255, 0), 2)'''
+    cv2.imshow("gt", gt)
+    '''
+    ret, thresh = cv2.threshold(gt, 127, 255, 0)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    gt = cv2.cvtColor(gt, cv2.COLOR_GRAY2BGR)
+    #xG,yG,wG,hG = cv2.boundingRect(cnts) will also need for loop?
+    cv2.drawContours(gt, cnts, -1, (0, 255, 0), 2)
+    cv2.imshow("gt", gt)
+    '''
     ret, thresh = cv2.threshold(damage, 127, 255, 0)
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    cv2.drawContours(damage, cnts, -1, (255, 0, 0), 2)
-    cv2.imshow("damage", damage)
+    damage = cv2.cvtColor(damage, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(damage, cnts, -1, (255, 0, 0), 2)#
+    #cv2.imshow("damage", damage)
 
     ret, thresh = cv2.threshold(bleaching, 127, 255, 0)
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    bleaching = cv2.cvtColor(bleaching, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(bleaching, cnts, -1, (255, 0, 255), 2)
-    cv2.imshow("bleaching", bleaching)
+    #cv2.imshow("bleaching", bleaching)
 
     ret, thresh = cv2.threshold(recovery, 127, 255, 0)
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    recovery = cv2.cvtColor(recovery, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(recovery, cnts, -1, (0, 255, 255), 2)
-    cv2.imshow("recovery", recovery)
+    #cv2.imshow("recovery", recovery)
 
 
-img1 = cv2.imread("images/coral1_mod.PNG")
-img2 = cv2.imread("images/coral2.PNG")
+img1 = cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/coral1_mod.png")
+img2 = cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/coral2.PNG")
 img1, img2 = trim_to_size(img1, img2)
 cv2.imshow("image1", resize(img1, 480))
-cv2.imshow("image2", resize(img2, 480))
+#cv2.imshow("image2", resize(img2, 480))
 img2_aligned = alignImages(img2, img1)
 cv2.imshow("aligned 2", resize(img2_aligned, 480))
-#img_diff = matrix_difference(img1, img3)
+img_diff = matrix_difference(img1, img2_aligned)
 #cv2.imshow("diff", resize(img_diff, 480))
 
 img1_w, img1_p = binarization(img1)
 img2_w, img2_p = binarization(img2_aligned)
 
-img3 = cv2.cvtColor(cv2.imread("images/identifytest_ref_white.PNG"), cv2.COLOR_BGR2GRAY)
-img4 = cv2.cvtColor(cv2.imread("images/identifytest_ref_pink.PNG"), cv2.COLOR_BGR2GRAY)
-img5 = cv2.cvtColor(cv2.imread("images/identifytest_new_white.PNG"), cv2.COLOR_BGR2GRAY)
-img6 = cv2.cvtColor(cv2.imread("images/identifytest_new_pink.PNG"), cv2.COLOR_BGR2GRAY)
+cv2.imshow("img1_w", img1_w)
+cv2.imshow("img2_w", img2_w)
+cv2.imshow("img1_p", img1_p)
+cv2.imshow("img2_p", img2_p)
 
-#classify_change_types(img1_w, img1_p, img2_w, img2_p) #Use this to test the new input
-classify_change_types(img3, img4, img5, img6) #Use this to test using sample input
+
+img3 = cv2.cvtColor(cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/identifytest_ref_white.png"), cv2.COLOR_BGR2GRAY) #/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images
+img4 = cv2.cvtColor(cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/identifytest_ref_pink.png"), cv2.COLOR_BGR2GRAY)
+img5 = cv2.cvtColor(cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/identifytest_new_white.png"), cv2.COLOR_BGR2GRAY)
+img6 = cv2.cvtColor(cv2.imread("/home/margot/Documents/uwrov/2018-2019-master/imagerecognition/images/identifytest_new_pink.png"), cv2.COLOR_BGR2GRAY)
+
+classify_change_types(img1_w, img1_p, img2_w, img2_p) #Use this to test the new input
+#classify_change_types(img3, img4, img5, img6) #Use this to test using sample input
 
 
 cv2.waitKey(0)
+
+cv2.destroyAllWindows()
