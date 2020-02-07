e88bbb368529c23f00d40074ee799692dc6fbd62
ajang304 <ajang304@uw.edu>
2020-02-06 17:24:28 -0800

    updated reactapp

--- Graphics Team/GUI/reactgui/src/MainCam.js
+++ Graphics Team/GUI/reactgui/src/MainCam.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import "./maincam.css"
 
 class MainCam extends React.Component {
-   let img_URL;
-
-
+   constructor() {
+      super();
+      this.img_URL = "http://10.18.207.212:8080/video";
+      this.cam_Num = 1;
+   }
 
    render() {
-      return <img src={img_URL} />;
+      let altText = "This is cam #: " + this.cam_Num;
+      return <img id="maincam" src={this.img_URL} alt={altText}></img>;
    }
 }
 
--- Graphics Team/GUI/reactgui/src/index.js
+++ Graphics Team/GUI/reactgui/src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import MainCam from "./MainCam";
 import * as serviceWorker from './serviceWorker';
 
-let d = new MainCam();
+ReactDOM.render(<MainCam />, document.getElementById("root"));
 
-ReactDOM.render(d.render(), document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
