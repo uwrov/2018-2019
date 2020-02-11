600b4c444437129637cc8428769487b860797043
Peter Gunarso <gunarp@cs.washington.edu>
2020-02-10 17:21:40 -0800

    cleanup

--- imagerecognition/2.1/detect_angle.py
+++ imagerecognition/2.1/detect_angle.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
-import math
 
-# This script will display video from a live feed, 
-# then it will find the angle between the vertical 
+# This script will display video from a live feed,
+# then it will find the angle between the vertical
 # and a blue line displayed on screen.
 
-# TODO: Find height of rov based on line distance - suggest elevation accordingly
-# TODO: Identify where in the screen the blue lines are - suggest strafing accordingly
+# TODO: Find height of rov based on line distance
+#       - suggest elevation accordingly
+# TODO: Identify where in the screen the blue lines are
+#       - suggest strafing accordingly
+
 
 def main():
     # Grab video feed from source
-    cap = cv2.VideoCapture('http://10.19.40.99:8080/video/mjpeg')
+    cap = cv2.VideoCapture('http://10.19.52.3:8080/video/mjpeg')
     height, width, _ = cap.read()[1].shape
 
     while True:
@@ -19,7 +21,7 @@ def main():
 
         # convert rgb image to hsv for color isolation
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
+
         # Set bounds for what shades of blue we want
         lower_blue = np.array([100, 150, 0])
         upper_blue = np.array([140, 255, 255])
@@ -38,14 +40,12 @@ def main():
         #     cv2.line(frame, (x, y+h//2), (x+w, y+h//2), (0,255,0), 2)
         #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~ Line approach ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Line approach ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         # Isolate lines
         edges = cv2.Canny(mask, 75, 150)
         lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=50)
-        llines = [] # Length of lines array
-
-        
+        llines = []  # Length of lines array
 
         if lines is not None:
             for line in lines:
@@ -53,36 +53,37 @@ def main():
                 # cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
                 # Calculate line distances and put them in array
-                lineDistance = np.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+                lineDistance = np.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
                 llines.append(lineDistance)
 
             # find and draw longest line
             i_max = llines.index(max(llines))
             x1, y1, x2, y2 = lines[i_max][0]
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 5)
-            
+
             dy = y2 - y1
             dx = x2 - x1
-            
+
             # Angle from vertical (clockwise - positive angle)
-            angle = np.arctan(dx/dy) * 180 / np.pi # output in degrees
-            cv2.putText(frame, str(angle), (5, height), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4, cv2.LINE_AA)
+            angle = np.arctan(dx/dy) * 180 / np.pi  # output in degrees
+            cv2.putText(frame, str(angle), (5, height), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 4, cv2.LINE_AA)
 
         # draw central vertical
-        cv2.line(frame, (width//2, 0), (width//2, height),(0,0,255),5) 
+        cv2.line(frame, (width//2, 0), (width//2, height), (0, 0, 255), 5)
 
         # Show camera image with line
         cv2.imshow('feed', frame)
         # cv2.imshow('mask', mask)
-    
+
         # Kill on escape key press
         if cv2.waitKey(1) == 27:
             break
 
-
     cv2.destroyAllWindows()
 
     cap.release()
+
 
 if __name__ == '__main__':
     main()
