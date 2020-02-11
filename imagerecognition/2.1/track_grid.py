028af440e68cddd880164d24b8befb437771a869
Peter Gunarso <gunarp@cs.washington.edu>
2020-02-10 17:58:06 -0800

    isolate pink

---
+++ imagerecognition/2.1/track_grid.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+# This script will track where the ROV is over a grid,
+# and give a visual representation of position.
+
+
+def main():
+    # Grab video feed from source
+    # cap = cv2.VideoCapture('http://10.19.52.3:8080/video/mjpeg')
+    # height, width, _ = cap.read()[1].shape
+
+    # while True:
+    # _, frame = cap.read()
+
+    im = cv2.imread('poopie.png')
+
+    # convert rgb image to hsv for color isolation
+    hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
+    # Set bounds for what shades of pink we want
+    lower_pink = np.array([130, 25, 160])
+    upper_pink = np.array([170, 255, 255])
+
+    # Isolate blue, draw lines on edges
+    mask = cv2.inRange(hsv, lower_pink, upper_pink)
+    mask = cv2.GaussianBlur(mask, (5, 5), 0)
+
+    cv2.imshow('hi', im)
+    cv2.imshow("bye", mask)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
