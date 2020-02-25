2e86d71798e606b34400c54e5d49bc6c41d48a3e
Peyton L <gabybiscuits@gmail.com>
2020-02-24 17:39:30 -0800

    Started implementing main parsing method in MainControl.java.

--- control/src/ROVControl/MainControl.java
+++ control/src/ROVControl/MainControl.java
@@ -8,7 +8,8 @@ import org.json.JSONObject;
 public class MainControl {
 
     public enum COMMANDS {
-        SET_VERTICAL_MOVEMENT
+        SET_VERTICAL_MOVEMENT,
+        SET_HORIZONTAL_MOVEMENT
     }
 
 
@@ -19,11 +20,33 @@ public class MainControl {
     /**
      * Changes a provided ROV State based on the commands.
      * @param commands an array of commands to apply, as JSON Objects
-     * @param state starting ROV state to modify.
+     * @param startingState starting ROV state to modify.
      * @return a new, modified ROV state.
      */
-    public ROVState parseServerInput(JSONObject[] commands, ROVState state) {
+    public ROVState parseServerInput(JSONObject[] commands, ROVState startingState) {
+        ROVState state = startingState.copy();
+        for (JSONObject command : commands) {
+            switch (COMMANDS.valueOf(command.getString("name"))) {
+                case SET_VERTICAL_MOVEMENT:
+                    setVerticalMovement(command, state);
+                    break;
+                case SET_HORIZONTAL_MOVEMENT:
 
+                default:
+            }
+        }
+
+        return state;
+    }
+
+    // Sets the vertical movement of a ROVState based on a SET_VERTICAL_MOVEMENT command.
+    // Requires that the command has an upward speed value.
+    private void setVerticalMovement(JSONObject command, ROVState state) {
+        if(command.has("upward_speed")) {
+            state.setVerticalSpeed(command.getFloat("upward_speed"));
+        } else {
+            throw new IllegalArgumentException("Command does not have upward_speed");
+        }
     }
 
     /**
