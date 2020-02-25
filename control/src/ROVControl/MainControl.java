18a6d5aeaba5344bd5c7df1bd2cc34652dd5e259
Peyton L <gabybiscuits@gmail.com>
2020-02-24 17:07:38 -0800

    Continued ROV Spec Writing for MainControl.java

--- control/src/ROVControl/MainControl.java
+++ control/src/ROVControl/MainControl.java
@@ -1,5 +1,7 @@
 package ROVControl;
 
+import org.json.JSONObject;
+
 /**
  * Runs the ROV Movement and Surface Communication.
  */
@@ -14,5 +16,25 @@ public class MainControl {
 
     }
 
+    /**
+     * Changes a provided ROV State based on the commands.
+     * @param commands an array of commands to apply, as JSON Objects
+     * @param state starting ROV state to modify.
+     * @return a new, modified ROV state.
+     */
+    public ROVState parseServerInput(JSONObject[] commands, ROVState state) {
+
+    }
+
+    /**
+     * Retrieves an array of commands from the server.
+     * @param address address of the server to retrieve data from
+     * @requires address is not null
+     * @throws java.io.IOException if server cannot be contacted
+     * @return an array of commands, represented by JSON Objects.
+     */
+    public JSONObject[] getInputFromServer(String address) {
+        return null;
+    }
 
 }
