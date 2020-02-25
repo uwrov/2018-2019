a06024d7b1ae78d1beca140f6e4693ca9ceac25c
Peyton L <gabybiscuits@gmail.com>
2020-02-24 16:24:18 -0800

    Continued ROV Spec Writing

--- control/src/ROVControl/MainControl.java
+++ control/src/ROVControl/MainControl.java
@@ -1,4 +1,18 @@
 package ROVControl;
 
+/**
+ * Runs the ROV Movement and Surface Communication.
+ */
 public class MainControl {
+
+    public enum COMMANDS {
+        SET_VERTICAL_MOVEMENT
+    }
+
+
+    public static void main (String[] args) {
+
+    }
+
+
 }
