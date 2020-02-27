5ba486dec20f36d8002ef5105c970f472c1d2075
alexOlshanskyy <51353059+alexOlshanskyy@users.noreply.github.com>
2020-02-26 16:06:07 -0800

    console and server
    server not working properly with get and post requests

--- Graphics Team/GUI/reactgui/src/index.js
+++ Graphics Team/GUI/reactgui/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Console from './Console.js';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Button from './Button';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-let button = new Button();
-
-ReactDOM.render(button.render(), document.getElementById('root'));
+//let button = new Button();
+ReactDOM.render(<Console />, document.getElementById('console'));
+//ReactDOM.render(button.render(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
