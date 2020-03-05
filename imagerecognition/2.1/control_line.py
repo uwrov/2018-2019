d18765a51c4105f66218ef8617a3034d277e8c41
jul001 <juchen101@gmail.com>
2020-03-04 16:47:19 -0800

    frame contains method

--- imagerecognition/2.1/control_line.py
+++ imagerecognition/2.1/control_line.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def main():
     # Grab video feed from source
     cap = cv2.VideoCapture("http://10.18.223.105:8080/video/mjpeg")
@@ -10,8 +11,6 @@ def main():
     while True:
         _, frame = cap.read()
         
-
-
         # convert rgb image to hsv for color isolation
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
@@ -28,32 +27,47 @@ def main():
         leftFrame = frame[height // 2 : height,  0 : width // 2]
         # cv2.imshow('mask', mask)
 
+        # two seperate frames, called twice
         right2 = rect_on_blue(right, rightFrame)
         left2 = rect_on_blue(left, leftFrame)
-
         cv2.imshow('r', right2)
         cv2.imshow('l', left2)
-        
+     
         if cv2.waitKey(27) == 1:
             break
 
         
 def rect_on_blue(mask, img):        
-        bluecnts = cv2.findContours(mask.copy(),
-                            cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-        for cnt in bluecnts:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.line(img, (x, y+h//2), (x+w, y+h//2), (0,0,255), 2)
-            cv2.drawContours(img, bluecnts, -1, 255, 3)
-            blue_area = max(bluecnts, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(blue_area)
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-        
-        return img
+    bluecnts = cv2.findContours(mask.copy(),
+                        cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)[-2]
+    for cnt in bluecnts:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.line(img, (x, y+h//2), (x+w, y+h//2), (0,0,255), 2)
+        cv2.drawContours(img, bluecnts, -1, 255, 3)
+        blue_area = max(bluecnts, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(blue_area)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+    
+        rightDistance = width - (x + w)
+        leftDistance = x
+        if leftDistance != rightDistance:
+            print ('false')
+        else:
+            print ('true')    
+    
+    return img
+
+
+
+
+    
             
+        
 
         
+
+    
         # if len(bluecnts) > 0:
         #     blue_area = max(bluecnts, key=cv2.contourArea)
         #     x, y, w, h = cv2.boundingRect(blue_area)
