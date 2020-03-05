0d9bd1583acf9cf66ad2e66432a9c1692eac437a
laurenk104 <lkrieger104@gmail.com>
2020-03-04 18:02:17 -0800

    get request

--- control/src/ROVControl/ROVServer.java
+++ control/src/ROVControl/ROVServer.java
@@ -1,6 +1,19 @@
 package ROVControl;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+
 import java.util.*;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.io.IOException;
 
 public class ROVServer {
    public final String IP_PORT;
@@ -10,19 +23,50 @@ public class ROVServer {
    }
 
    // returns a http get response from the server with this subdirectory
-   public String getRequest(String subDir) {
-      URL serverURL = new URL("http://" + IP_PORT + subDir);
+   public String getRequest(String subDir, String[] params) throws IOException {
+      if (params.length % 2 == 1) {
+         throw new IllegalArgumentException("Parameters do not have param/value pairs");
+      }
+
+      String parameters = "?";
+      for (int i = 0; i < params.length; i += 2) {
+         parameters += params[i] + "=" + params[i + 1] + "&";
+      }
+      parameters = parameters.substring(0, parameters.length() - 1);
+
+      URL serverURL = new URL("http://" + IP_PORT + subDir + parameters);
       HttpURLConnection connection = (HttpURLConnection) serverURL.openConnection();
       connection.setRequestMethod("GET");
 
-      Map<String, String> parameters = new HashMap<>();
-      parameters.put("param1", "val");
+      connection.setConnectTimeout(1000);
+      connection.setReadTimeout(1000);
 
-      con.setDoOutput(true);
-      DataOutputStream out = new DataOutputStream(con.getOutputStream());
-      out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-      out.flush();
-      out.close();
+      // Reading the response
+      int status = connection.getResponseCode();
+
+      Reader streamReader = null;
+      if (status > 299) {
+         System.err.println("connection error!");
+         streamReader = new InputStreamReader(connection.getErrorStream());
+      } else {
+         streamReader = new InputStreamReader(connection.getInputStream());
+      }
+
+      BufferedReader in = new BufferedReader(streamReader);
+      String inputLine;
+      StringBuffer content = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+         content.append(inputLine);
+      }
+      in.close();
+
+      connection.disconnect();
+
+      return content.toString();
+   }
+
+   public String getRequest(String subDir) throws IOException{
+      return getRequest(subDir, new String[0]);
    }
 
 /**
@@ -33,28 +77,11 @@ public class ROVServer {
       connection.setRequestMethod("POST");
    }
 */
-/*   public ArrayList<JSONObject> getCommands() {
-      // getrequest("/commands")
+   public JSONArray getCommands() throws IOException {
+      JSONParser jsonParser = new JSONParser();
+      Object obj = jsonParser.parse(getRequest("/commands"));
+      JSONArray list = (JSONArray) obj;
+      return list;
    }
-   */
 
-}
-
-public class ParameterStringBuilder {
-    public static String getParamsString(Map<String, String> params)
-      throws UnsupportedEncodingException{
-        StringBuilder result = new StringBuilder();
-
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-          result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-          result.append("=");
-          result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-          result.append("&");
-        }
-
-        String resultString = result.toString();
-        return resultString.length() > 0
-          ? resultString.substring(0, resultString.length() - 1)
-          : resultString;
-    }
 }
