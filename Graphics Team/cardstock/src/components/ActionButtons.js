4360c82594d808fdcc6c752ef2a1d5c6c39ffb54
Andrew <anemnox@gmail.com>
2020-05-19 18:52:14 -0700

    Action Button components

---
+++ Graphics Team/cardstock/src/components/ActionButtons.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ActionButtons.css";
+
+class ActionButtons extends React.Component {
+
+   //
+   // Properties to be handling:
+   // this.props.onSkipTurn:
+   //
+   render() {
+      return (
+         <div className="actionbuttons">
+            <div className="skipButton" onClick={this.handleSkip}>
+               Skip turn!
+            </div>
+         </div>
+      );
+   }
+
+   handleSkip = () => {
+      if(this.props.onSkipTurn instanceof Function) {
+         this.props.onSkipturn();
+      } else {
+         console.log("Error: no function was supplied!");
+      }
+   }
+}
+
+export default ActionButtons;
