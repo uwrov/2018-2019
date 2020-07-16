d3148f3fb152265a3d94963007c8b638f13500a4
ungjus <ungjus19@uw.edu>
2020-07-15 17:17:59 -0700

    plt cleared

--- Graphics Team/GUI/test server/gui_game_server.py
+++ Graphics Team/GUI/test server/gui_game_server.py
@@ -469,6 +469,8 @@ def reset_server():
     show_results=False
     playing_game=False
     player_list.clear()
+    end_player_results.clear()
+    plt.clear()
     send_game_state()
 
 @sio.on("Get Results")
