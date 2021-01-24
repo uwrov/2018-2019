add099462992b35e1800e7431ce90c22b16e8783
lkrieg <lkrieg@uw.edu>
2021-01-23 18:56:47 -0800

    added comments

--- imagerecognition/2.1/identify_shapes.py
+++ imagerecognition/2.1/identify_shapes.py
@@ -26,40 +26,43 @@ def identify_shapes(row):
 
     i = 0
     for img in row:
-        shape = process_img(img, str(i))
+        shape = process_img(img)
         names.append(shape)
         i = i + 1
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    print(names)
     return names
 
-def process_img(img, name):
+def process_img(img):
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # get centermost pixel grayscale color
     (h, w) = grayscale.shape[:2]
     height = int(h/2)
     width = int(w/2)
     color = grayscale[height, width]
 
-    thresh = 185
+    # convert image to binary (black background, white shape)
+    thresh = 185 #might be worth changing in the future, since this won't always be the background color
     _, binary = cv2.threshold(grayscale, thresh, 255, cv2.THRESH_BINARY)
-    if color < thresh - 10:
+    if color < thresh - 15:
         thresh = color + 15
         _, binary = cv2.threshold(grayscale, thresh, 255, cv2.THRESH_BINARY_INV)
     else:
-        if color < 195 and color > 175:
+        if color < thresh + 15 and color > thresh - 15:
             thresh = 255
         else:
             thresh = color - 15
         _, binary = cv2.threshold(grayscale, thresh, 255, cv2.THRESH_BINARY)
 
+    # get contours of binary image
     cnts = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
-    print(name + " - binary")
-
+    # name the shape
     shape = "empty"
     for c in cnts:
         shape = shape_detector(c);
@@ -77,7 +80,6 @@ def shape_detector(cnts):
     else:
         shape = "idk fam"
 
-    print(shape)
     return shape
 
 if __name__ == '__main__':
